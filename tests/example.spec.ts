39adb69851688b513c6c5f4c48cb3245c8712ebf
Brian Rodriguez <63204156+bridanrod@users.noreply.github.com>
2026-02-25 17:04:24 -0300

    undo changes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,5 +16,3 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
-//Este es un cambio hecho por nati
-// Este es un cambio hecho por Brian
